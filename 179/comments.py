863e77b4b845386956c5b1a1936fcdbd7a51e570
Cedric Young <ccedricyoung@gmail.com>
2021-08-31 17:08:19 -0700

    fixed

--- 179/comments.py
+++ 179/comments.py
@@ -1,24 +1,24 @@
 import re
 
 inputs = [
-    '''
-    def hello_world():
-        # A simple comment preceding a simple print statement
-        print("Hello World")
-    ''',
-    '''
-    def say_hello(name):
-        """A simple function that says hello... Richie style"""
-        print(f"Hello {name}, is it me you're looking for?")
-    ''',
-    '''
-    class SimpleClass:
-        """Class docstrings go here."""
+    # '''
+    # def hello_world():
+    #     # A simple comment preceding a simple print statement
+    #     print("Hello World")  # say hi
+    # ''',
+    # '''
+    # def say_hello(name):
+    #     """A simple function that says hello... Richie style"""
+    #     print(f"Hello {name}, is it me you're looking for?")
+    # ''',
+    # '''
+    # class SimpleClass:
+    #     """Class docstrings go here."""
     
-        def say_hello(self, name: str):
-            """Class method docstrings go here."""
-            print(f'Hello {name}')
-    ''',
+    #     def say_hello(self, name: str):
+    #         """Class method docstrings go here."""
+    #         print(f'Hello {name}')
+    # ''',
     '''
     def __init__(self, name, sound, num_legs):
         """
@@ -38,16 +38,28 @@ inputs = [
 ]
 
 def strip_comments(code):
-    # remove whole line strip_comments r'\s*#.*$\n' => ''
-    
-    code = re.sub(r'\s*#.*$\n', '', code)
-    
-    # remove side comments 
-    code = re.sub(r'  #.*$', '', code)
+    result = []
+    is_docstring = False
+    for line in code.splitlines():
+        doc_parts = len(line.split('"""'))
+        if doc_parts == 3:
+            continue
 
-    print(code)    
-    return code
+        if doc_parts == 2:
+            is_docstring = not is_docstring
+            continue
 
-if __file__ ==  '__main__':
-    for line in inputs:
-        strip_comments(line)
+        if is_docstring:
+            continue
+
+        # remove whole line comment
+        if re.match(r'^\s*#.*$', line):
+            continue
+
+        result.append(re.sub(r'  #.*$', '', line))
+
+    print('\n'.join(result))
+    return '\n'.join(result)
+
+for line in inputs:
+    strip_comments(line)
