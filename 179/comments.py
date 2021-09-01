6d3aa608277ecc251047e83231add5460586e3f8
Cedric Young <ccedricyoung@gmail.com>
2021-08-31 19:26:38 -0700

    implement with one big regex

--- 179/comments.py
+++ 179/comments.py
@@ -19,26 +19,37 @@ inputs = [
     #         """Class method docstrings go here."""
     #         print(f'Hello {name}')
     # ''',
+    # '''
+    # def __init__(self, name, sound, num_legs):
+    #     """
+    #     Parameters
+    #     ----------
+    #     name : str
+    #         The name of the animal
+    #     sound : str
+    #         The sound the animal makes
+    #     num_legs : int, optional
+    #         The number of legs the animal (default is 4)
+    #     """
+    #     self.name = name
+    #     self.sound = sound
+    #     self.num_legs = num_legs
+    # ''',
     '''
-    def __init__(self, name, sound, num_legs):
-        """
-        Parameters
-        ----------
-        name : str
-            The name of the animal
-        sound : str
-            The sound the animal makes
-        num_legs : int, optional
-            The number of legs the animal (default is 4)
-        """
-        self.name = name
-        self.sound = sound
-        self.num_legs = num_legs
+"""this is
+my awesome script
+"""
+# importing modules
+import re
+
+def hello(name):
+    """my function docstring"""
+    return f'hello {name}'  # my inline comment
     ''',
 ]
 
 def strip_comments(code):
-    return re.sub(r'(\n\s*# .*|  #.*|\n\s*"""(.|\n)*?""")\n', '\n', code, re.MULTILINE)
+    return re.sub(r'(\n\s*?# .*|  #.*|\n\s*"""(.|\n)*?""")(?=\n)', '', code, re.MULTILINE)
 
 for line in inputs:
     print(strip_comments(line))
