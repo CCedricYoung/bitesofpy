e8cf215eb2bbeb874ae849d4edc58917e60d44e3
Cedric Young <ccedricyoung@gmail.com>
2021-09-03 11:15:50 -0700

    split 224 solved

---
+++ 224/split.py
@@ -0,0 +1,6 @@
+import re
+
+def get_sentences(text):
+    """Return a list of sentences as extracted from the text passed in.
+       A sentence starts with [A-Z] and ends with [.?!]"""
+    return re.split(r'(?<=[.?!]) (?=[A-Z])', text.replace('\n', ' ').strip())
