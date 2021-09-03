93dd0ae0e2503fee138d698af188ae3472170dbf
Cedric Young <ccedricyoung@gmail.com>
2021-09-03 11:21:12 -0700

    add comment

--- 224/split.py
+++ 224/split.py
@@ -3,4 +3,6 @@ import re
 def get_sentences(text):
     """Return a list of sentences as extracted from the text passed in.
        A sentence starts with [A-Z] and ends with [.?!]"""
+    
+    # split on the space between sentences
     return re.split(r'(?<=[.?!]) (?=[A-Z])', text.replace('\n', ' ').strip())
