7dd5ea3eb9583b311e3e9bbee3d8d27b3c801cd8
Cedric Young <ccedricyoung@gmail.com>
2021-09-29 10:42:10 -0700

    332 search_apartment

---
+++ 332/search_apartment.py
@@ -0,0 +1,37 @@
+from typing import List
+
+EAST = "E"
+WEST = "W"
+
+
+def search_apartment(buildings: List[int], direction: str) -> List[int]:
+    """
+    Find and return the indices of those building with
+    the desired view: EAST (E) or WEST (W).
+
+    See sample inputs / outputs below and in the tests.
+    """
+    if direction == 'W':
+        indexes = range(len(buildings))
+    else:
+        indexes = range(len(buildings) - 1, -1, -1)
+
+    tallest = 0
+    results = []
+    for x in indexes:
+        if buildings[x] > tallest:
+            tallest = buildings[x]
+            results.append(x)
+
+    return sorted(results)
+
+if __name__ == "__main__":
+    A = [3, 5, 4, 4, 7, 1, 3, 2]  # central tallest
+    B = [1, 1, 1, 1, 1, 2]  # almost flat
+    #
+    #  W <-                    ->  E(ast)
+    #
+    print(search_apartment(A, "W"))  # [0, 1, 4]
+    print(search_apartment(A, "E"))  # [4, 6, 7]
+    print(search_apartment(B, "W"))  # [0, 5]
+    print(search_apartment(B, "E"))  # [5]
