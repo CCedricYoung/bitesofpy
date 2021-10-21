b6b8b9c890da018820705dc9271d62a20bdcda10
Cedric Young <ccedricyoung@gmail.com>
2021-10-20 19:40:19 -0700

    76 count_down

--- 76/count_down.py
+++ 76/count_down.py
@@ -6,42 +6,26 @@ def count_down(counter):
     raise ValueError
 
 
-@count_down.register
+@count_down.register(str)
 def _(counter: str):
     for x in reversed(range(1, len(counter) + 1)):
         print(counter[:x])
 
 
-@count_down.register
-def _(counter: int):
+@count_down.register(int)
+@count_down.register(float)
+def _(counter):
     count_down(str(counter))
 
 
-@count_down.register
-def _(counter: float):
-    count_down(str(counter))
-
-
-@count_down.register
+@count_down.register(list)
 def _(counter: list):
     count_down("".join(map(str, counter)))
 
 
-@count_down.register
-def _(counter: set):
-    count_down(list(counter))
-
-
-@count_down.register
-def _(counter: tuple):
-    count_down(list(counter))
-
-
-@count_down.register
-def _(counter: dict):
-    count_down(list(counter.keys()))
-
-
-@count_down.register
-def _(counter: range):
+@count_down.register(set)
+@count_down.register(tuple)
+@count_down.register(dict)
+@count_down.register(range)
+def _(counter):
     count_down(list(counter))
